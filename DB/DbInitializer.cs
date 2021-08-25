1921044aa34957b4edff14b069804465b021e325
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-08-25 10:03:25 -0300

    Context and Initialezer added

---
+++ DB/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using RG_Potter_API.DB;
+using RG_Potter_API.Models;
+using System;
+
+namespace RG_Potter_API
+{
+    internal class DbInitializer
+    {
+        private readonly bool _reset;
+
+        public DbInitializer(IConfiguration configuration)
+        {
+            _reset = bool.Parse(configuration["ResetDB"] ?? "true");
+        }
+
+        internal void Initialize(PotterContext context)
+        {
+            if (_reset) context.Database.EnsureDeleted();
+
+            if (context.Database.EnsureCreated()) return;
+
+            var users = new[]
+            {
+                new User
+                {
+
+                }
+            };
+
+            foreach (var user in users) context.Users.Add(user);
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DB/PotterContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RG_Potter_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RG_Potter_API.DB
+{
+    public class PotterContext : DbContext
+    {
+        public PotterContext(DbContextOptions<PotterContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
