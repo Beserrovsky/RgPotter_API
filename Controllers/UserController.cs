9e25c679c368f4f332014752a0717a79d67d4392
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-08-25 12:07:43 -0300

    CRUD methods

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,28 +1,111 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using RG_Potter_API.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RG_Potter_API.DB;
+using RG_Potter_API.Models;
 
 namespace RG_Potter_API.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class UserController : ControllerBase
     {
-        private readonly ILogger<UserController> _logger;
+        private readonly PotterContext _context;
 
-        public UserController(ILogger<UserController> logger)
+        public UserController(PotterContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
+        // GET: api/User
         [HttpGet]
-        public User Get(Guid id)
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            throw new NotImplementedException();
+            var users = _context.Users.Include(c => c.House).AsNoTracking();
+            return await users.ToListAsync();
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(Guid id)
+        {
+            var user = _context.Users.Include(c => c.House).FirstOrDefaultAsync(i => i.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return await user;
+        }
+
+        // PUT: api/User/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(Guid id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/User
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // DELETE: api/User/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<User>> DeleteUser(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        private bool UserExists(Guid id)
+        {
+            return _context.Users.Any(e => e.Id == id);
         }
     }
 }
