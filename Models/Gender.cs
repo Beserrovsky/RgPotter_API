817bfe3e799366b0a12ef9ab3d2d205b36889bc3
felipebeserra25@gmail.com <felipebeserra25@gmail.com>
2021-09-13 13:54:05 -0300

    Models improvement

---
+++ Models/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RG_Potter_API.Models
+{
+    public class Gender
+    {
+        public char Pronoum { get; set; }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -15,8 +15,14 @@ namespace RG_Potter_API.Models
 
         public House House { get; set; }
 
+        public char Pronoum { get; set; }
+
         public string Email { get; set; }
 
         public string Password { get; set; }
+
+        public int LumusSuccesses { get; set; }
+
+        public int LumusFails { get; set; }
     }
 }
